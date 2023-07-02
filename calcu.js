cfa756f243cd422a72551c0ae80aea8fa5406da1
Mary Jonadith Chua <136776364+maryjonachua@users.noreply.github.com>
2023-07-03 07:48:50 +0800

    Create calcu.js

---
+++ calcu.js
@@ -0,0 +1,27 @@
+let data = document.getElementById('display');
+let buttons = document.querySelectorAll('button');
+
+let calculation = "";
+let arrayButton = Array.from(buttons);
+arrayButton.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            calculation = eval(calculation);
+            data.value =calculation ;
+        }
+
+        else if(e.target.innerHTML == 'C'){
+            calculation = "";
+            data.value = calculation;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            calculation = calculation.substring(0, calculation.length-1);
+            data.value = calculation;
+        }
+        else{
+            calculation += e.target.innerHTML;
+            data.value = calculation;
+        }
+        
+    })
+})
